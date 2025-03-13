914765eb73a40529c75e1a70d0af8ffd9d41241e
Mike <112992524+x59215@users.noreply.github.com>
2025-03-13 12:23:28 -0400

    Update Most Lucrative Products.sql

--- Easy/Most Lucrative Products.sql
+++ Easy/Most Lucrative Products.sql
@@ -1,9 +1,8 @@
 /*You have been asked to find the 5 most lucrative products in terms of total revenue for the first half of 2022 (from January to June inclusive).
-
-
 Output their IDs and the total revenue.
 
 Table: online_orders */
+-- https://platform.stratascratch.com/coding/2119-most-lucrative-products?code_type=1
 
 select 
     product_id,
