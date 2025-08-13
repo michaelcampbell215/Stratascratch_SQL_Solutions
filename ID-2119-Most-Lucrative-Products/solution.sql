f0b33f6c884c15f9f0d63485fd9b4ba8fc8692c1
mcam215 <mcam215@gmail.com>
2025-08-13 18:47:56 -0400

    solution added

--- ID-2119-Most-Lucrative-Products/solution.sql
+++
@@ -1,7 +0,0 @@
-select 
-    product_id,
-    sum(cost_in_dollars * units_sold) as revenue
-from online_orders
-group by 1
-order by 2 desc
-limit 5
--- ID-9663-Most-Profitable-Financial-Company/solution.sql
+++ ID-9663-Most-Profitable-Financial-Company/solution.sql
@@ -1,5 +1,13 @@
-select 
-    company,
-    continent
-from forbes_global_2010_2014
-limit 1;
+with cte as 
+    (select 
+        company, 
+        profits, 
+        sector, 
+        continent, R
+        OW_NUMBER() OVER(PARTITION BY sector ORDER BY profits  DESC) AS row_num
+    from forbes_global_2010_2014)
+
+select company, continent
+from cte
+where sector = 'Financials'
+limit 1
